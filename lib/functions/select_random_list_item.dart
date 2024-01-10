31fc650e9af1410307f99d9942cfc91e78f917a2
Xavier-MD <xavier.pmd@outlook.com>
2024-01-09 22:10:13 -0500

    Finalizing homepage layout.

--- lib/functions/select_random_list_item.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:math';
-
-T selectRandomListItem<T>(List<T> list, Random random) {
-  int randomNumber = random.nextInt(list.length);
-  return list[randomNumber];
-}
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,10 +1,10 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:fortune_cat/functions/select_random_list_item.dart';
 import 'package:fortune_cat/models/asset_lists.dart';
 import 'package:fortune_cat/models/text_lists.dart';
 import 'package:fortune_cat/ui/components/fc_svg.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -69,48 +69,140 @@ class _HomePageState extends State<HomePage> {
     initializeAssets();
   }
 
+  Widget catAssetLayer(String assetPath) {
+    return Image(
+      fit: BoxFit.contain,
+      image: AssetImage(assetPath),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+        minimum: EdgeInsets.fromLTRB(5.0.w, 8.0.w, 5.0.w, 0),
         child: Flex(
           direction: Axis.vertical,
           children: [
+            // Cat Picture Section
             Flexible(
               flex: 7,
-              child: Container(
-                color: Colors.red,
+              child: Column(
+                children: [
+                  Flexible(
+                    flex: 2,
+                    fit: FlexFit.tight,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        FcSvg(
+                          assetName: 'assets/vectors/icons/tarot_icon.svg',
+                          width: 10.0.w,
+                          color: Colors.black,
+                        ),
+                        FcSvg(
+                          assetName: 'assets/vectors/text/home_text.svg',
+                          width: 40.0.w,
+                          color: Colors.black,
+                        ),
+                        FcSvg(
+                          assetName: 'assets/vectors/icons/settings_icon.svg',
+                          width: 10.0.w,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Flexible(
+                    flex: 10,
+                    child: Stack(
+                      children: [
+                        catAssetLayer(selectedVibe),
+                        catAssetLayer(selectedCat),
+                        catAssetLayer(selectedEyeAccessory),
+                        catAssetLayer(selectedHeadAccessory),
+                        catAssetLayer(selectedPawAccessory),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
 
             // Fortune Quote Section
             Flexible(
-              flex: 4,
-              child: Column(
+              flex: 3,
+              child: SizedBox(
+                width: 80.0.w,
+                child: Column(
+                  children: [
+                    FcSvg(
+                        assetName:
+                            'assets/vectors/decor/quote_upper_bracket.svg'),
+                    const Spacer(),
+                    SizedBox(
+                      width: 70.0.w,
+                      child: Text(
+                        selectedQuote,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const Spacer(),
+                    FcSvg(
+                        assetName:
+                            'assets/vectors/decor/quote_lower_bracket.svg'),
+                  ],
+                ),
+              ),
+            ),
+
+            // Third Section
+            Flexible(
+              flex: 2,
+              fit: FlexFit.tight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   FcSvg(
-                      assetName:
-                          'assets/vectors/decor/quote_upper_bracket.svg'),
-                  const Spacer(),
-                  Text(selectedQuote),
-                  const Spacer(),
+                    assetName: 'assets/vectors/icons/cat_icon.svg',
+                    width: 10.0.w,
+                    color: Colors.black,
+                  ),
+                  Chip(
+                    label: Text('writing'),
+                  ),
                   FcSvg(
-                      assetName:
-                          'assets/vectors/decor/quote_lower_bracket.svg'),
+                    assetName: 'assets/vectors/icons/tarot_icon.svg',
+                    width: 10.0.w,
+                    color: Colors.black,
+                  ),
                 ],
               ),
             ),
 
+            // Fourth Section
             Flexible(
               flex: 2,
-              child: Container(
-                color: Colors.yellow,
-              ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Container(
-                color: Colors.pink,
+              fit: FlexFit.tight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: (() => {}),
+                    child: Text('1'),
+                  ),
+                  ElevatedButton(
+                    onPressed: (() => {}),
+                    child: Text('2'),
+                  ),
+                  ElevatedButton(
+                    onPressed: (() => {}),
+                    child: Text('3'),
+                  ),
+                ],
               ),
             ),
           ],
