8a789ada798d35aaa3887a31bace400664128168
Xavier-MD <xavier.pmd@outlook.com>
2024-01-13 17:01:16 -0500

    Updated asset list file with new assets.

--- lib/models/asset_lists.dart
+++ lib/models/asset_lists.dart
@@ -45,14 +45,10 @@ Asset assetObjectCreator(String name, String imagePath, Rarity rarity) {
 
 class AssetLists {
   static List<Asset> backgroundList = [
-    assetObjectCreator('Blank Background',
-        'assets/images/backgrounds/blank_background.png', Rarity.common),
-    assetObjectCreator('Blue Background',
-        'assets/images/backgrounds/blue_background.png', Rarity.common),
-    assetObjectCreator('Coral Background',
-        'assets/images/backgrounds/coral_background.png', Rarity.common),
-    assetObjectCreator('Pink Background',
-        'assets/images/backgrounds/pink_background.png', Rarity.common),
+    assetObjectCreator('Blue Branches Background',
+        'assets/images/backgrounds/main/blue_branches.png', Rarity.common),
+    assetObjectCreator('Night Sky Background',
+        'assets/images/backgrounds/main/night_sky.png', Rarity.uncommon),
     // Add more backgrounds as needed
   ];
 
@@ -81,8 +77,10 @@ class AssetLists {
   ];
 
   static List<Asset> eyeList = [
-    assetObjectCreator('Blank Eyes',
-        'assets/images/backgrounds/blank_background.png', Rarity.ordinary),
+    assetObjectCreator(
+        'Default Eyes',
+        'assets/images/backgrounds/main/transparent_canvas.png',
+        Rarity.ordinary),
     assetObjectCreator('Glasses 1',
         'assets/images/accessories/eyes/glasses_1.png', Rarity.common),
     assetObjectCreator('Monocle 1',
@@ -94,13 +92,17 @@ class AssetLists {
 
   static List<Asset> headList = [
     assetObjectCreator(
-        'Blank Head', 'assets/images/backgrounds/blank_background.png', Rarity.ordinary),
+        'Default Head',
+        'assets/images/backgrounds/main/transparent_canvas.png',
+        Rarity.ordinary),
     // Add more head accessories as needed
   ];
 
   static List<Asset> pawList = [
     assetObjectCreator(
-        'Blank Paws', 'assets/images/backgrounds/blank_background.png', Rarity.ordinary),
+        'Default Paws',
+        'assets/images/backgrounds/main/transparent_canvas.png',
+        Rarity.ordinary),
     assetObjectCreator(
         'Book 1', 'assets/images/accessories/paws/book_1.png', Rarity.common),
     assetObjectCreator('Drink 1', 'assets/images/accessories/paws/drink_1.png',
@@ -114,15 +116,17 @@ class AssetLists {
 
   static List<Asset> vibeList = [
     assetObjectCreator(
-        'Blank Vibe', 'assets/images/backgrounds/blank_background.png', Rarity.ordinary),
+        'Blank Vibe',
+        'assets/images/backgrounds/main/transparent_canvas.png',
+        Rarity.ordinary),
     assetObjectCreator('Leaves 1',
-        'assets/images/accessories/vibes/leaves_1.png', Rarity.uncommon),
+        'assets/images/backgrounds/accents/leaves_1.png', Rarity.uncommon),
     assetObjectCreator(
-        'Rain 1', 'assets/images/accessories/vibes/rain_1.png', Rarity.rare),
+        'Rain 1', 'assets/images/backgrounds/accents/rain_1.png', Rarity.rare),
     assetObjectCreator('Rainbow 1',
-        'assets/images/accessories/vibes/rainbow_1.png', Rarity.fabled),
+        'assets/images/backgrounds/accents/rainbow_1.png', Rarity.fabled),
     assetObjectCreator('Sparkles 1',
-        'assets/images/accessories/vibes/sparkles_1.png', Rarity.unique),
+        'assets/images/backgrounds/accents/sparkles_1.png', Rarity.unique),
     // Add more vibes as needed
   ];
 }
